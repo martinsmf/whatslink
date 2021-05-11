e1a8f9bf42903ef57ff1d65451c3dd3040ff19ba
Matheus F Martins <martinsmf00@gmail.com>
2021-05-11 01:19:58 -0300

    QaDev init

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="dashboard">
+    <h1>Seu gerenciador digital de contatos</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'dashboard'
+}
+</script>
