16d1c6f917c27ebf6c625ce00743ef08b18da1f3
Matheus F Martins <martinsmf00@gmail.com>
2021-05-26 00:08:01 -0300

    Post
    post para registrar um contato criado no backend.

--- backend/controllers/contact.controle.js
+++ backend/controllers/contact.controle.js
@@ -1,6 +1,18 @@
 const ContactModel = require('../models/contact.model')
 
 module.exports = {
+
+    async register(request, h) {
+
+        const contact = new ContactModel({
+            name: request.payload.name,
+            number: request.payload.number,
+            description: request.payload.description
+        })
+        contact.save()
+        return null
+    },
+
     async list(request, h) {
         const contacts = await ContactModel.find().exec()
         return contacts
